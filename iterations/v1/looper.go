2e83e2c273a9817fc36e193df8b70a0232e6f79c
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-02-14 19:56:40 +0530

    pointers and erros

--- iterations/v1/looper.go
+++ iterations/v1/looper.go
@@ -1,9 +1,22 @@
-package iterations
+package main
 
-func Looper(char string) string {
-	var repeated string
-	for i := 0; i < 5; i++ {
-		repeated = repeated + char
+import (
+	"fmt"
+)
+
+func Repeater(num []int) int {
+
+	sum := 0
+	for _, i := range num {
+		sum += i
 	}
-	return repeated
+	return sum
+}
+
+func main() {
+	sli := []string{"anay", "vinu", "vinayaka", "vinayaka"}
+
+	fmt.Println(Repeater([]int{10, 100, 400, 500}))
+
+	fmt.Printf("The rest element is %s\n", sli[0:2])
 }
---
+++ pointeranderrors/go.mod
@@ -0,0 +1,3 @@
+module github.com/abhishekamralkar/learn-go-with-tests/pointeranderrors
+
+go 1.20
